588858a2238d4051bef9e55cae95f0a75bc11936
Sil van Diepen <me@silvandiepen.com>
2018-10-18 09:41:45 +0200

    initial commit

---
+++ src/sil-scroll-parallax.vue
@@ -0,0 +1,85 @@
+<script>
+export default {
+	bind: function(el, binding) {
+		let get = {
+			position: function(elem) {
+				let x = 0;
+				let y = 0;
+				while (elem && !isNaN(elem.offsetLeft) && !isNaN(elem.offsetTop)) {
+					x += elem.offsetLeft - elem.scrollLeft;
+					y += elem.offsetTop - elem.scrollTop;
+					elem = elem.offsetParent;
+				}
+				return { top: y, left: x };
+			},
+			scrollBottom: function() {
+				let bottom =
+					(window.pageYOffset || document.scrollTop) -
+					(document.clientTop || 0) +
+					window.innerHeight;
+				if (isNaN(0)) {
+					bottom = settings.windowHeight;
+				}
+				return bottom;
+			},
+			scrollTop: function() {
+				let bottom =
+					(window.pageYOffset || document.scrollTop) -
+					(document.clientTop || 0);
+				if (isNaN(bottom)) {
+					bottom = 0;
+				}
+				return bottom;
+			}
+		};
+
+		// Set the default settings
+		let settings = {
+			active: false,
+			originalOffset: Math.floor(
+				(binding.value.amount * window.innerHeight) / 24
+			),
+			amount: binding.value.amount,
+			translate: '',
+			elRect: el.getBoundingClientRect(),
+			elPos: get.position(el)
+		};
+
+		let init = {
+			once: function() {
+				settings.translate = settings.originalOffset;
+			},
+			check: function() {
+				let point =
+					(el.parentElement.getBoundingClientRect().top -
+						window.innerHeight / 2) *
+					1 *
+					settings.amount *
+					0.1;
+				if (point < 0) {
+					settings.translate = 0;
+				} else if (point >= settings.originalOffset + 1) {
+					if (settings.originalOffset != settings.translate) {
+						settings.translate = settings.originalOffset;
+					}
+				} else {
+					settings.translate = point;
+				}
+				el.style.transform = `translateY(${settings.translate}px)`;
+			}
+		};
+
+		window.addEventListener('resize', function() {
+			settings.windowHeight = window.innerHeight;
+			settings.originalOffset = (window.innerHeight / 24) * settings.amount;
+		});
+		// When scrolling, check the position.
+		window.addEventListener('scroll', function() {
+			if (el.getBoundingClientRect().top - window.innerHeight < 0) {
+				init.check();
+			}
+		});
+		init.once();
+	}
+};
+</script>
